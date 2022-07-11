20ab891f74704a563a53d9f02176800e7a0b69f9
Govedarski <velislavgovedarski@gmail.com>
2022-07-11 20:51:39 +0300

    01AsynchronousProgramming Exercise

--- 01AsynchronousProgramming/Exercise/06.Blog/app.js
+++ 01AsynchronousProgramming/Exercise/06.Blog/app.js
@@ -1,5 +1,58 @@
+import {html, render} from 'https://unpkg.com/lit-html?module';
+
 function attachEvents() {
-    console.log('TODO...');
+    const buttonLoad = document.getElementById('btnLoadPosts');
+    const buttonView = document.getElementById('btnViewPost')
+    const selectContainer = document.getElementById('posts')
+    const postTitle = document.getElementById('post-title')
+    const postBody = document.getElementById('post-body')
+    const commentList = document.getElementById('post-comments')
+
+    buttonLoad.addEventListener('click', loadPosts);
+
+    function loadPosts(e) {
+        fetch('http://localhost:3030/jsonstore/blog/posts')
+            .then(res => res.json())
+            .then(data => showOptions(Object.values(data)));
+    }
+
+    function showOptions(data) {
+        selectContainer.innerHTML = ''
+        data.forEach(postData =>{
+            selectContainer.innerHTML +=`<option value="${postData.id}">${postData.title}</option>`
+        });
+    }
+
+    buttonView.addEventListener('click', loadContent)
+
+    function loadContent() {
+        const postId = selectContainer.value
+        fetch('http://localhost:3030/jsonstore/blog/posts')
+            .then(res => res.json())
+            .then(data => showPost(Object.values(data)));
+
+        fetch('http://localhost:3030/jsonstore/blog/comments')
+            .then(res => res.json())
+            .then(data =>{
+                data = Object.values(data).filter(x=>x.postId === postId)
+                showComments(data)
+            })
+    }
+
+    function showPost(data) {
+        const post = data.find(x=>x.id === selectContainer.value)
+        console.log(post)
+        postTitle.textContent = post.title
+        postBody.textContent = post.body
+    }
+
+    function showComments(data) {
+        commentList.innerHTML = ''
+        console.log(data)
+        data.forEach(c=> {
+            commentList.innerHTML += `<li id=${c.id}>${c.text}</li>`
+        })
+    }
 }
 
 attachEvents();
