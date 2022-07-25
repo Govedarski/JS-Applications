f6e64dfb1e1f070e841c77fbdd1e02e4204e747d
Govedarski <velislavgovedarski@gmail.com>
2022-07-25 15:23:44 +0300

    Fill dropdown solution

--- 05ClientSideRendering/Exercise/04.Fill-Dropdown/dropdown.js
+++ 05ClientSideRendering/Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,36 @@
-function addItem() {
-    console.log('TODO:...');
+import {html, render} from '../../../node_modules/lit-html/lit-html.js';
+import {repeat} from '../../../node_modules/lit-html/directives/repeat.js';
+
+
+const containerSelect = document.getElementById('menu');
+const optionTemplate = (data) => html`<option value="${data.text}">${data.text}</option>`;
+const optionsTemplate = (data) => html`${repeat(data, d => d._id, optionTemplate)}`;
+
+
+async function loadOptions() {
+    const optionsData = Object.values(await fetch('http://localhost:3030/jsonstore/advanced/dropdown')
+        .then(response => response.json()));
+    render(optionsTemplate(optionsData), containerSelect);
 }
+
+await loadOptions();
+
+async function addItem(e) {
+    e.preventDefault();
+    const text = new FormData(e.target).get('text');
+    e.target.reset()
+
+    const optionData = await fetch('http://localhost:3030/jsonstore/advanced/dropdown',
+        {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({text})
+        })
+        .then(response => response.json());
+
+    const tempContainer = document.createDocumentFragment()
+    render(optionTemplate(optionData),tempContainer)
+    containerSelect.appendChild(tempContainer)
+}
+
+document.querySelector('form').addEventListener('submit', addItem);
