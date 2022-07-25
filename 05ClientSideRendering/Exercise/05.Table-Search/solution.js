65e55aa038ab3338371578faa27da91d8bc77f19
Govedarski <velislavgovedarski@gmail.com>
2022-07-25 16:01:35 +0300

    Table search solution

--- 05ClientSideRendering/Exercise/05.Table-Search/solution.js
+++ 05ClientSideRendering/Exercise/05.Table-Search/solution.js
@@ -1,8 +1,40 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import {html, render} from '../../../node_modules/lit-html/lit-html.js';
+import {repeat} from '../../../node_modules/lit-html/directives/repeat.js';
+import {classMap} from '../../../node_modules/lit-html/directives/class-map.js';
 
-   function onClick() {
-      //   TODO:
+async function solve() {
+    const tbodyContainer = document.querySelector('tbody');
+    const searchField = document.getElementById('searchField');
 
-   }
+    const rowTemplate = (rowData) => html`
+        <tr class=${rowData.classes ? classMap(rowData.classes) : null}>
+            <td>${rowData.firstName} ${rowData.lastName}</td>
+            <td>${rowData.email}</td>
+            <td>${rowData.course}</td>
+        </tr>`;
+    const tbodyContentTemplate = (rowsData) => html`${repeat(rowsData, rowData => rowsData._id, rowTemplate)}`;
+
+    const data = Object.values(await fetch('http://localhost:3030/jsonstore/advanced/table')
+        .then(response => response.json()));
+
+    console.log(data)
+    render(tbodyContentTemplate(data), tbodyContainer);
+
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+    function onClick() {
+        const searchText = searchField.value.toLowerCase();
+        data.forEach(obj =>
+            obj.classes = {
+                select: Object.values(obj)
+                    .slice(0, 5)
+                    .some(value => value.toLowerCase().includes(searchText)),
+            });
+
+        render(tbodyContentTemplate(data), tbodyContainer);
+    }
 }
+
+solve();
+
+
